2f52e7548af24941c1e69b36aae126b66a20b3e9
umin <adorecamus@gmail.com>
2023-12-22 18:39:34 +0900

    User Service 인터페이스와 구현체 작성, Controller 구현

---
+++ src/main/kotlin/com/teamsparta/courseregistration/domain/user/service/UserService.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.courseregistration.domain.user.service
+
+import com.teamsparta.courseregistration.domain.user.dto.ProfileResponse
+import com.teamsparta.courseregistration.domain.user.dto.SignUpRequest
+import com.teamsparta.courseregistration.domain.user.dto.UpdateUserProfileRequest
+import com.teamsparta.courseregistration.domain.user.dto.UserResponse
+
+interface UserService {
+
+    fun signUp(request: SignUpRequest): UserResponse
+
+    fun updateUserProfile(userId: Long, request: UpdateUserProfileRequest): ProfileResponse
+}
